ca9232de4073036b75989aa6d92116ef61378260
Smyja <akposlive59@gmail.com>
2022-05-23 07:22:50 +0100

    update

--- djql/schema.py
+++ djql/schema.py
@@ -1,1 +1,16 @@
-from 
+import graphene
+from graphene_django import DjangoObjectType
+from djql.models import Restaurant
+
+class RestaurantType(DjangoObjectType):
+    class Meta:
+        model = Restaurant
+        fields = ("id", "name", "address")
+
+class Query(graphene.ObjectType):
+    restaurants = graphene.List(RestaurantType)
+
+    def resolve_restaurants(self, info, **kwargs):
+        return Restaurant.objects.all()
+
+schema = graphene.Schema(query=Query)
